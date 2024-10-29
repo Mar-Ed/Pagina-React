f2a90fe43308fc3f6500d3144b2f6b5c956e849c
Mar-Ed <158010261+Mar-Ed@users.noreply.github.com>
2024-10-28 23:23:00 -0500

    primer commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{html,js}",
+    "./components/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#3490dc',
+        'secondary': '#ffed4a',
+      },
+    },
+  },
+  plugins: [],
+}
+
