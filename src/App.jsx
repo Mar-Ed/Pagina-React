179813040a07cfcb257d18cdd600cb8a52115b1c
Mar-Ed <158010261+Mar-Ed@users.noreply.github.com>
2024-10-29 20:29:09 -0500

    avance2

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import Button from "./components/Button";
 
+import Bloques from "./components/bloques";
+import  Layaut  from "./components/Layaut";
+import  Navbar  from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <div  className="bg-slate-800">
-        <h1 className="text-3xl font-bold underline bg-slate-500">Hello world!
-          <Button name="Marcelo"/>
-          <Button name="Rebeca"/>
-          <Button name="Kim"/>
-
-        </h1>
+      <div className="bg-slate-800 min-h-screen">
+        <Navbar>  </Navbar>
+        <Layaut>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor impedit, corporis aliquid deserunt tempore quisquam similique maiores nisi odit reiciendis quia harum aperiam fugit temporibus labore omnis illum dicta recusandae commodi quae magni mollitia natus tenetur? Laudantium atque sint eveniet perspiciatis eaque recusandae, modi iusto vel adipisci eius. Impedit, harum.</p>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor impedit, corporis aliquid deserunt tempore quisquam similique maiores nisi odit reiciendis quia harum aperiam fugit temporibus labore omnis illum dicta recusandae commodi quae magni mollitia natus tenetur? Laudantium atque sint eveniet perspiciatis eaque recusandae, modi iusto vel adipisci eius. Impedit, harum.</p>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor impedit, corporis aliquid deserunt tempore quisquam similique maiores nisi odit reiciendis quia harum aperiam fugit temporibus labore omnis illum dicta recusandae commodi quae magni mollitia natus tenetur? Laudantium atque sint eveniet perspiciatis eaque recusandae, modi iusto vel adipisci eius. Impedit, harum.</p>
+        </Layaut>
+        <Bloques/>
       </div>
     </>
   );
